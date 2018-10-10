85634f721896cc61050ec0c18ca70d8931a06d17
Valery Yushchenko <valery.yushchenko@gmail.com>
2018-10-10 12:40:14 +0300

    totals, better html using Pug templates, styling, logo/favicon

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var Web3 = require('web3');
 var web3 = new Web3(new Web3.providers.HttpProvider('https://mainnet.infura.io/'));
+const path = require('path');
 
 var pulls = {
 	'0x7dddf3bc31dd30526fc72d0c73e99528c1a4a011': 0, // token sales
@@ -14,15 +15,15 @@ var pulls = {
 }
 
 var purpose = {
-	'0x7dddf3bc31dd30526fc72d0c73e99528c1a4a011': 'token sales',
-	'0xa77273cba38b587c05defac6ac564f910472900e': 'reserve',
-	'0x2df67b4d71b4cfca4b90cc048641c36102602937': 'team',
-	'0xabc463fad37a01ac6d9752ed99fdef80f7316faf': 'advisors',
-	'0x973a0d5a68081497769e4794e58ca64b020dc164': 'bounty',
-	'0xcf2a0f05e94058155d836d07556ad7f4be24b461': 'advisor 1',
-	'0x079b671d1b65c5be3c47f028031f52e7d91cbb6f': 'advisor 2',
-	'0xef2f5bd8f54159d29d89f2f6f01574dd721f4ff2': 'advisor 3',
-	'0xdf63a620fdc1d0c05417de76e425e8809129b32a': 'advisor 4'
+	'0x7dddf3bc31dd30526fc72d0c73e99528c1a4a011': 'Sharpay Token Sale',
+	'0xa77273cba38b587c05defac6ac564f910472900e': 'Sharpay System Reserve',
+	'0x2df67b4d71b4cfca4b90cc048641c36102602937': 'Sharpay Team Bonus',
+	'0xabc463fad37a01ac6d9752ed99fdef80f7316faf': 'Sharpay Advisors',
+	'0x973a0d5a68081497769e4794e58ca64b020dc164': 'Sharpay Bountry',
+	'0xcf2a0f05e94058155d836d07556ad7f4be24b461': 'Sharpay Advisor 1',
+	'0x079b671d1b65c5be3c47f028031f52e7d91cbb6f': 'Sharpay Advisor 2',
+	'0xef2f5bd8f54159d29d89f2f6f01574dd721f4ff2': 'Sharpay Advisor 3',
+	'0xdf63a620fdc1d0c05417de76e425e8809129b32a': 'Sharpay Advisor 4'
 }
 
 var contractAbi= [{"constant":true,"inputs":[{"name":"_addr","type":"address"},{"name":"_index","type":"uint256"}],"name":"getFreezing","outputs":[{"name":"_release","type":"uint64"},{"name":"_balance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_addr","type":"address"}],"name":"freezingCount","outputs":[{"name":"count","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"}];
@@ -60,47 +61,43 @@ async function getHolds() {
 
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 3000;
+
+app.set('view engine', 'pug');
+app.use('/public', express.static(path.join(__dirname, 'public')));
+// app.use(express.static('public'));
 
 app.get('/', async function (req, res) {
 
-	var holds = await getHolds();
-	var prev = '';
-	var dates = {};
-	var content = '<div>';
-
-	content += '<h2>Sharpay token freezing:</h2>';
+	let holds = await getHolds(),
+			tokenSupply = 4000000000,
+			tokenFrozen = 0,
+			current = { addr: null },
+			releasesByDate = {},
+			releasesByAddress = [];
 
 	holds.forEach((h) => {
+
 		let fd = h.dt.getFullYear() + '-' + ('0' + (h.dt.getMonth() + 1)).slice(-2) + '-' + ('0' + h.dt.getDate()).slice(-2);
 
-		if( prev != h.addr ) {
-			if( prev != '' ) {
-				content += '\n';
-			}
-			content += '<h5>' + h.addr + ' - ' + h.purpose + '</h5>';
-			prev = h.addr;
+		if( current.addr != h.addr ) {
+			current = { addr: h.addr, purpose: h.purpose, releases: {} };
+			releasesByAddress.push(current);
 		}
 
-		content += '<div>&nbsp;&nbsp;' + fd + ' -> ' + h.balance + '</div>';
+		current.releases[fd] = h.balance;
 
-		if( dates[ fd ] ) {
-			dates[ fd ] = parseInt( h.balance ) + dates[ fd ];
+		if( releasesByDate[ fd ] ) {
+			releasesByDate[ fd ] = parseInt( h.balance ) + releasesByDate[ fd ];
 		} else {
-			dates[ fd ] = parseInt( h.balance );
+			releasesByDate[ fd ] = parseInt( h.balance );
 		}
+
+		tokenFrozen += parseInt(h.balance);
 
 	});
 
-	content += '<h2>Sharpay token release:</h2>';
-
-	for( var d in dates ) {
-		content += '<div>&nbsp;&nbsp;' + d + ' -> ' + dates[d] + '</div>';
-	}
-
-	content += '</div>';
-
-  res.send(content);
+  res.render('index', { tokenSupply, tokenFrozen, releasesByAddress, releasesByDate });
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Listening on port ${port}!`));
